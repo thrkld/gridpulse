9136db1cda950ce837ee1e23123459b3846227c0
James Thirkeld <jamesthirkeld12345@gmail.com>
2026-06-16 19:59:04 +0100

    See last commit

--- src/gridpulse/clients/carbonIntensity.py
+++ src/gridpulse/clients/carbonIntensity.py
@@ -1,51 +1,22 @@
 import requests
+from datetime import datetime, date,timezone
 headers = {
   'Accept': 'application/json'
 }
 
-# This is the data for one half hour
-r = requests.get('https://api.carbonintensity.org.uk/regional', params={}, headers = headers)
-regionalData = r.json()['data'][0]
+# Ingests a single instance of the raw regional, national and generation data for carbon intensity
+def carbonIntensityIngestion() -> dict:
+  r = requests.get('https://api.carbonintensity.org.uk/regional', params={}, headers = headers)
+  n = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
+  g = requests.get('https://api.carbonintensity.org.uk/generation', params={}, headers = headers)
 
-n = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
-g = requests.get('https://api.carbonintensity.org.uk/generation', params={}, headers = headers)
-nationalData = n.json()['data'][0]
-generationData = g.json()['data']
+  regionalData = r.json()['data']
+  nationalData = n.json()['data']
+  generationData = g.json()['data']
 
-
-regional = [
-    {
-        "from" : regionalData["from"],
-        "to" : regionalData["to"],
-        "regionID" : r["regionid"],
-        "shortName" : r["shortname"],
-        "forecast" : r["intensity"]["forecast"],
-        "index" : r["intensity"]["index"],
-        "generationMix" : r["generationmix"]
-    }
-    for r in regionalData["regions"]
-]
-
-for row in regional:
-    print(row)
-
-national = [
-    {
-        "from" : nationalData["from"],
-        "to" : nationalData["to"],
-        "forecast" : nationalData["intensity"]["forecast"],
-        "actual" : nationalData["intensity"]["actual"],
-        "index" : nationalData["intensity"]["index"]
-    }
-]
-
-generation = [
-    {
-        "from" : generationData["from"],
-        "to" : generationData["to"],
-        "generationMix" : generationData["generationmix"]
-    }
-]
-
-for row in national:
-    print(row)
+  return {
+    "ingestedUTC" : datetime.now(timezone.utc).isoformat,
+    regionalData : r.json(),
+    nationalData : n.json(),
+    generationData : g.json()
+  }
