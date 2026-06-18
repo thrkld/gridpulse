a9a4f357d014b97f1da71baba8cf2fa08ceeced4
James Thirkeld <jamesthirkeld12345@gmail.com>
2026-06-18 22:21:46 +0100

    refactor: separate carbon intensity and elexon endpoints into independent fetch functions and implemented some more safety in request calls (through timeout and raise_for_status)

--- src/gridpulse/clients/carbon_intensity.py
+++ src/gridpulse/clients/carbon_intensity.py
@@ -1,18 +1,31 @@
 import requests
 from datetime import datetime, date,timezone
+
 headers = {
   'Accept': 'application/json'
 }
-
-# Ingests a single instance of the raw regional, national and generation data for carbon intensity
-def carbon_intensity_ingestion() -> dict:
-  r = requests.get('https://api.carbonintensity.org.uk/regional', params={}, headers = headers)
-  n = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
-  g = requests.get('https://api.carbonintensity.org.uk/generation', params={}, headers = headers)
-
-  return {
+# Ingests a single instance of the raw regional, national or generation data for carbon intensity
+def fetch_regional_ci() -> dict:
+  r = requests.get('https://api.carbonintensity.org.uk/regional', headers=headers, timeout=30)
+  r.raise_for_status()
+  return{
     "ingested_utc" : datetime.now(timezone.utc).isoformat(),
-    "regional_data" : r.json(),
-    "national_data" : n.json(),
-    "generation_data" : g.json()
+    "payload" : r.json()
   }
+
+def fetch_national_ci() -> dict:
+  n = requests.get('https://api.carbonintensity.org.uk/intensity', headers=headers, timeout=30)
+  n.raise_for_status()
+  return{
+    "ingested_utc" : datetime.now(timezone.utc).isoformat(),
+    "payload" : n.json()
+  }
+
+def fetch_generation_ci() -> dict:
+  g = requests.get('https://api.carbonintensity.org.uk/generation', headers=headers, timeout=30)
+  g.raise_for_status()
+  return{
+    "ingested_utc" : datetime.now(timezone.utc).isoformat(),
+    "payload" : g.json()
+  }
+
