fa762e2d77ef48e42d7554142d0d551b06d453e3
James Thirkeld <jamesthirkeld12345@gmail.com>
2026-07-17 16:33:38 +0100

    feat: dagster sweeps created and smoke-tested

--- src/gridpulse/orchestration/definitions.py
+++ src/gridpulse/orchestration/definitions.py
@@ -1,7 +1,14 @@
 from dagster import Definitions, asset, ScheduleDefinition
 
-from gridpulse.ingest.run_carbon_intensity import run_latest as ci_run_latest
-from gridpulse.ingest.run_elexon import run_latest as elexon_run_latest
+from gridpulse.ingest.run_carbon_intensity import (
+    run_latest as ci_run_latest,
+    run_sweep as ci_run_sweep,
+)
+from gridpulse.ingest.run_elexon import (
+    run_latest as elexon_run_latest,
+    run_sweep_initial as elexon_run_sweep_initial,
+    run_sweep_interim as elexon_run_sweep_interim,
+)
 from gridpulse.ingest.run_neso import run_latest as neso_run_latest
 
 
@@ -11,8 +18,23 @@ def carbon_intensity_latest_raw():
 
 
 @asset
+def carbon_intensity_sweep_raw():
+    ci_run_sweep()
+
+
+@asset
 def elexon_latest_raw():
     elexon_run_latest()
+
+
+@asset
+def elexon_sweep_initial_raw():
+    elexon_run_sweep_initial()
+
+
+@asset
+def elexon_sweep_interim_raw():
+    elexon_run_sweep_interim()
 
 
 @asset
@@ -29,12 +51,38 @@ half_hourly_schedule = ScheduleDefinition(
 
 twice_daily_schedule = ScheduleDefinition(
     name="twice_daily_refresh",
-    cron_schedule="0 10,22 * * *",  # Runs 10am and 10pm
+    cron_schedule="0 10,22 * * *",  # Runs 10:00 and 22:00 UTC
     target=[neso_latest_raw],
     execution_timezone="UTC",
 )
 
+daily_schedule = ScheduleDefinition(
+    name="daily_sweep",
+    cron_schedule="15 0 * * *",  # Runs daily at 00:15 UTC
+    target=[carbon_intensity_sweep_raw, elexon_sweep_interim_raw],
+    execution_timezone="UTC",
+)
+
+weekly_schedule = ScheduleDefinition(
+    name="weekly_sweep",
+    cron_schedule="45 0 * * 0",  # Runs weekly at 00:45 UTC
+    target=[elexon_sweep_initial_raw],
+    execution_timezone="UTC",
+)
+
 defs = Definitions(
-    schedules=[half_hourly_schedule, twice_daily_schedule],
-    assets=[carbon_intensity_latest_raw, elexon_latest_raw, neso_latest_raw],
+    schedules=[
+        half_hourly_schedule,
+        twice_daily_schedule,
+        daily_schedule,
+        weekly_schedule,
+    ],
+    assets=[
+        carbon_intensity_latest_raw,
+        carbon_intensity_sweep_raw,
+        elexon_latest_raw,
+        elexon_sweep_initial_raw,
+        elexon_sweep_interim_raw,
+        neso_latest_raw,
+    ],
 )
